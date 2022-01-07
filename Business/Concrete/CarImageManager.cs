6137d6b5bc35ffaf2cf68a406d5ce35ee3551079
Nurdan Mamuş <nurdanmamus@gmail.com>
2022-01-07 11:36:33 +0300

    Adding Car Images

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,79 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Business;
+using Core.Utilities.Helpers.FileHelper;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService 
+    {
+        ICarImageDal _carImageDal;
+        IFileHelper  _fileHelper;
+
+        public CarImageManager(ICarImageDal carImageDal, IFileHelper fileHelper)
+        {
+            _carImageDal = carImageDal;
+            _fileHelper  = fileHelper; 
+        }
+
+        public IResult Add(IFormFile file, CarImage carImage)
+        {
+            IResult result = BusinessRules.Run(CheckIfCarImageLimit(carImage.CarId));
+            if (result!=null)
+            {
+                return result;
+            }
+            carImage.ImagePath = _fileHelper.Upload(file, PathConstants.ImagesPath);
+            carImage.Date = DateTime.Now;
+            _carImageDal.Add(carImage); 
+            return new SuccessResult("Photo is upload successfully");
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            _fileHelper.Delete(PathConstants.ImagesPath + carImage.ImagePath);
+            _carImageDal.Delete(carImage);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetByCarId(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<CarImage> GetByImageId(int imageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(IFormFile file, CarImage carImage)
+        {
+            carImage.ImagePath = _fileHelper.Update(file, PathConstants.ImagesPath + carImage.ImagePath, PathConstants.ImagesPath);
+            _carImageDal.Update(carImage);
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCarImageLimit(int carId)
+        {
+            var result = _carImageDal.GetAll(c => c.CarId == carId).Count;
+            if (result>=5)
+            {
+                return new ErrorResult();
+            }
+            return new SuccessResult();
+        }
+    }
+}
